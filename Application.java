1bdac2130c2811e70ad45fe8544ff1f7f1c08fd3
BuildTools <unconfigured@null.spigotmc.org>
2019-02-15 21:07:53 -0500

    Added alignment, Removed GUI

--- Application.java
+++ Application.java
@@ -1,38 +1,11 @@
- import java.awt.*;
- import java.awt.event.*;
- import javax.swing.*;
-
-
 public class Application
 {
-
-
 
 	public static void main(String[] args)
 	{
 
-		String[] list = {"hey","bye"};
+		Character test = new Character();
+		System.out.println(test.alignment);
 
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run() 
-			{
-
-
-				
-				JFrame frame = new JFrame("DND Simulation");
-				JList characters = new JList(list);
-				characters.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-				characters.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-				characters.setVisibleRowCount(-1);
-				JScrollPane listScroller = new JScrollPane(characters);
-				frame.setSize(500,500);
-				frame.getContentPane().add(characters, "West");
-
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
 	}
 }
--- Character.java
+++ Character.java
@@ -1,9 +1,21 @@
-public class Character()
+public class Character
 {
 	//Dwarf, Elf, Gnome, Half-Elf, Hafling, Half-Orc, Human, Tiefling
-	String firstName, lastName;
+	String firstName, lastName, alignment;
 	public Character()
 	{
+		this.alignment = generateAlignment();
+	}
 
+	public static String generateAlignment()
+	{
+		int axesNum = (int) (Math.random() * 3); //Lawful, Neutral, Chaotic
+		int moralityNum = (int) (Math.random() * 3); //Good, Neutral, Evil
+
+		String[] axes = {"Lawful", "Neutral", "Chaotic"};
+		String[] morality = {"Good","Neutral","Evil"};
+
+		if (axesNum == 1 && moralityNum == 1) return "True Neutral";
+		else return axes[axesNum] + " " + morality[moralityNum];
 	}
 }
